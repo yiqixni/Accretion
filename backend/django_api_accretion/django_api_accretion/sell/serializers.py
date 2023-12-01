d3b4f57d1edf722d45a304fe12f186f10a5c10bb
yxni <yxni@mit.edu>
2023-12-01 14:07:17 -0500

    backend DRF add single image upload function

--- backend/django_api_accretion/django_api_accretion/sell/serializers.py
+++ backend/django_api_accretion/django_api_accretion/sell/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import SellInfo  
 
 class SellInfoSerializer(serializers.ModelSerializer):
+    images = serializers.ImageField(max_length=255, allow_empty_file=False, allow_null=True, required=False)
+    
     class Meta:
         model = SellInfo 
-        fields = ["address", "description", "pictures", 
+        fields = ["address", "description", "images", 
                   "asking_price", "shares"] 
+        
+    def get_pictures_url(self, obj):
+        return self.context['request'].build_absolute_uri( obj.images.url ) 
         
         
