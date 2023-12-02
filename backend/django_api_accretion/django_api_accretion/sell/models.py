16935c055c4df511b6ce5a0a9ba8cdd69b731ea8
yxni <yxni@mit.edu>
2023-12-02 01:20:46 -0500

    multiple image upload back basic feature is complete

--- backend/django_api_accretion/django_api_accretion/sell/models.py
+++ backend/django_api_accretion/django_api_accretion/sell/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create SellInfo 
-class SellInfo(models.Model):
+class PropertyInfo(models.Model):
     address = models.CharField(max_length=100)
     description = models.CharField(max_length=255) 
-    images = models.ImageField(upload_to="property_images", 
-                                 default="property_images/none/NoImage.jpg")
     asking_price = models.DecimalField(max_digits=10, decimal_places=2) 
     shares = models.IntegerField() 
+    
+class PropertyImage(models.Model):
+    property_info = models.ForeignKey(PropertyInfo, 
+                                      on_delete=models.CASCADE, 
+                                      related_name="images")
+    image = models.ImageField(upload_to="property_images/") 
+
+    
 
     
